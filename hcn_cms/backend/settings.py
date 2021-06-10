2f07d0956292cdbefa132593c645855b09f40e22
Ndifreke <ndy40.ekott@gmail.com>
2021-06-10 22:12:56 +0100

    HCN-1005 - add support for memcache
    - Adding memcache server to docker-composer file
    - Update django to use server for cache
    
    fixes HCN-1005

--- hcn_cms/backend/settings.py
+++ hcn_cms/backend/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '{server}:{port}'.format(server=os.environ['CACHE_SERVER'], port=os.environ['CACHE_SERVER_PORT'])
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
