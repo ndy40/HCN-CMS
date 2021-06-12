a1f1d8229de40e0301e518bb60f2f1255bf90dc8
Ndifreke <ndy40.ekott@gmail.com>
2021-06-12 19:39:39 +0100

    HCN-1002 - Adding indexes

--- hcn_cms/sermons/models.py
+++ hcn_cms/sermons/models.py
@@ -6,10 +6,10 @@ from django.db import models
 class Series(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True)
-    starts_at = models.DateField(null=True)
-    ends_at = models.DateField(null=True)
+    starts_at = models.DateField(null=True, db_index=True)
+    ends_at = models.DateField(null=True, db_index=True)
     cover_image = models.ImageField(upload_to='series/%Y/%m/')
-    tags = models.JSONField(null=True)
+    tags = models.JSONField(null=True, db_index=True)
 
 
 class Sermon(models.Model):
@@ -19,15 +19,15 @@ class Sermon(models.Model):
     """
     title = models.CharField(max_length=255)
     preacher = models.CharField(max_length=225)
-    mime_type = models.CharField(max_length=255)
+    mime_type = models.CharField(max_length=255, db_index=True)
     url = models.URLField()
     size = models.IntegerField(null=True)
     description = models.TextField(null=True)
-    tags = models.JSONField(null=True)
+    tags = models.JSONField(null=True, db_index=True)
     likes = models.PositiveIntegerField(default=0, null=True)
-    published = models.DateTimeField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(null=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
     thumbnail = models.ImageField(upload_to='sermons/thumbs/%Y/%m/')
-    series = models.ForeignKey('Series', on_delete=models.SET_NULL, null=True)
+    series = models.ForeignKey('Series', on_delete=models.SET_NULL, null=True, db_index=True)
 
