762cd092f8e85dfef120b618044869e70168043c
Ndifreke <ndy40.ekott@gmail.com>
2021-06-20 22:02:18 +0100

    HCN-1003 Added support for Django Rest Framework
    Added support for Django rest framework.

--- hcn_cms/backend/settings.py
+++ hcn_cms/backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sermons.apps.SermonsConfig',
-    'tagging'
+    'tagging',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,9 @@ STATIC_ROOT = '/app/hcn_cms/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
