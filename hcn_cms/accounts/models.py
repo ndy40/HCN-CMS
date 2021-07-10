12c2185efd79c16f0aa7f9b35b91d0e99ec15829
Ndifreke <ndy40.ekott@gmail.com>
2021-07-10 23:08:26 +0100

    HCN-1018 Added new `User` model.

--- hcn_cms/accounts/models.py
+++ hcn_cms/accounts/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    class Meta:
+        unique_together = ['email', 'is_staff']
 
 
 class Device(models.Model):
@@ -19,6 +25,7 @@ class Device(models.Model):
     screen_height = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now=True, null=True)
     status = models.CharField(max_length=255, choices=DEVICE_STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='devices')
 
     @property
     def is_active(self):
@@ -28,3 +35,11 @@ class Device(models.Model):
 class DeviceConsent(models.Model):
     push_notification = models.BooleanField(default=True)
     device = models.OneToOneField(Device, on_delete=models.CASCADE, primary_key=True, related_name='consent')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    is_member = models.BooleanField(null=True, default=False)
+    year_joined = models.DateField(null=True, blank=True)
+    reset_token = models.CharField(max_length=255, null=True, blank=True)
+    rest_token_expires = models.DateTimeField(null=True, blank=True)
