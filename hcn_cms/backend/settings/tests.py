1772f587bd908bbde4e86f16cf2115ef615ba888
Ndifreke <ndy40.ekott@gmail.com>
2021-08-30 04:42:39 +0100

    HCN-1025 - adding bookmark modules.
    - fixed tests - db name resolution.
    - changed caching to local memory during tests.

--- hcn_cms/backend/settings/tests.py
+++ hcn_cms/backend/settings/tests.py
@@ -7,3 +7,15 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['PG_DATABASE'],
+        'USER': os.environ['PG_USER'],
+        'PASSWORD': os.environ['PG_PASSWORD'],
+        'HOST': '127.0.0.1',  # os.environ['PG_HOST'],
+        'PORT': '5432',
+    }
+}
