550b3ae11599876b998b9955c98671bc0c0c4c42
Ndifreke <ndy40.ekott@gmail.com>
2022-03-22 21:44:30 +0000

    HCN-1038 adding mailhog service for catching emails
    Service to catch and view emails.

--- hcn_cms/accounts/selectors.py
+++ hcn_cms/accounts/selectors.py
@@ -1,4 +1,5 @@
-from .models import Device
+from .models import Device, User
+from django_rest_passwordreset.models import ResetPasswordToken
 
 
 def get_device_by_id(*, mac_address: str) -> None or Device:
@@ -6,3 +7,13 @@ def get_device_by_id(*, mac_address: str) -> None or Device:
         return Device.objects.get(token=mac_address)
     except Device.DoesNotExist:
         pass
+
+
+def get_user_from_token(token: str):
+    try:
+        token = ResetPasswordToken.object.get(key=token)
+        
+    except ResetPasswordToken.DoesNotExist:
+        pass
+
+
--- hcn_cms/accounts/views.py
+++ hcn_cms/accounts/views.py
@@ -1,3 +1,16 @@
 # from django.shortcuts import render
-
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+@api_view(['GET'])
+def password_reset_view(request, token):
+    # 1. validate the token and make sure it isn't expired.
+    # 2. if token has expired or invalid, show error message
+    # 3. if valid token then show reset form and embed token within
+    # 4. validate form data, and update password.
+
+    pass
+
